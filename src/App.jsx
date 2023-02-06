aeb821a8a8a2af055b2f67d3882d72a2915f3091
priegelsdev <priegelsdev@gmail.com>
2023-02-06 10:47:37 +0100

    add some header styles with box shadow

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon as fasMoon } from '@fortawesome/free-solid-svg-icons'
+import { faMoon as farMoon } from '@fortawesome/free-regular-svg-icons'
 
 export default function App() {
+  // state for light/dark mode
+  const [lightMode, setLightMode] = useState(true)
+
   return (
-    <div>
-      
+    <div className="app-container">
+      <header>
+        <h3 className="header-title">Where in the world?</h3>
+
+        {/* logic to display dark or light mode */}
+        <div className="color-switch">
+          <FontAwesomeIcon icon={lightMode ? farMoon : fasMoon} />
+          <span>{lightMode ? 'Dark Mode' : 'Light Mode'}</span> 
+        </div>    
+      </header>
+
     </div>
   )
 }
