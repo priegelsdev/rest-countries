e1132b775d62a495fc658337c7ad077648509876
priegelsdev <priegelsdev@gmail.com>
2023-02-06 16:05:33 +0100

    add further styling to country view

--- src/CountryView.jsx
+++ src/CountryView.jsx
@@ -9,19 +9,23 @@ export default function CountryView(props) {
   const currencies = props.country.currencies.map(currency => currency.name)
   const languages = props.country.languages.map(language => language.name)
 
+  /* these should be spelled out */
+
   const borderCountries = props.country.borders.map(border => (
     <button key={crypto.randomUUID()} className="border-btn">{border}</button>
   ))
 
   return (
     <div className="country-view-container">
-      <button className="back-btn"><FontAwesomeIcon icon={fasArrowLeftLong}/>Back</button>
-      <img src={props.country.flag} alt={`flag of ${props.country.name}`} />
+      <button className="back-btn"><FontAwesomeIcon className="back-btn-icon" icon={fasArrowLeftLong}/>Back</button>
+      <img className="flag" src={props.country.flag} alt={`flag of ${props.country.name}`} />
 
       <div className="country-info">
         <h2 className="country-name">{props.country.name}</h2>
+        <p className="country-region"><b>Native Name: </b>{props.country.nativeName}</p>
         <p className="country-population"><b>Population: </b>{newFor}</p>
         <p className="country-region"><b>Region: </b>{props.country.region}</p>
+        <p className="country-region"><b>Sub Region: </b>{props.country.subregion}</p>
         <p className="country-capital"><b>Capital: </b>{props.country.capital}</p>
       </div>
 
@@ -33,7 +37,9 @@ export default function CountryView(props) {
 
       <div className="border-countries">
         <h3 className="border-title">Border Countries</h3>
-        {borderCountries}
+        <div className="border-btn-container">
+          {borderCountries}
+        </div>
       </div>
     </div>
   )
