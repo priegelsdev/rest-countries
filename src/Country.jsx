4798b4dc23f342760e73205d0539251fbab137c9
priegelsdev <priegelsdev@gmail.com>
2023-02-08 10:48:50 +0100

    make country name clickable

--- src/Country.jsx
+++ src/Country.jsx
@@ -7,7 +7,7 @@ export default function Country(props) {
     <div className="country-container" id={!props.lightMode ? 'dark-el' : 'none'}>
       <img className="flag flag-clickable" src={props.flag} onClick={() => props.onClick(props.numericCode)}></img>
       <div className="country-details">
-        <h2 className="country-name">{props.name}</h2>
+        <h2 className="country-name flag-clickable" onClick={() => props.onClick(props.numericCode)}>{props.name}</h2>
         <p className="country-population"><b>Population: </b>{newFor}</p>
         <p className="country-region"><b>Region: </b>{props.region}</p>
         <p className="country-capital"><b>Capital: </b>{props.capital}</p>
